73bb1a7a62eca3720a2983f2eb443445bd54be9d
epmakOFF <viktor@epmakoff.ru>
2024-08-25 07:29:13 +0500

    Create choice and chech inputs

--- v2/install.sh
+++ v2/install.sh
@@ -3,24 +3,86 @@
 curl -fsSL https://get.docker.com -o get-docker.sh  
 sh get-docker.sh
 
-if [ -n "$1" ]
+restore_config ()
+{
+clear
+echo Restore config
+cat <<EOF >.env
+POSTGRES_DB=keycloak_db
+POSTGRES_USER=keycloak_db_user
+POSTGRES_PASSWORD=keycloak_db_user_password
+KEYCLOAK_ADMIN=admin
+KEYCLOAK_ADMIN_PASSWORD=password
+EMAIL=CHANGE_ME
+EOF
+exit
+}
+
+
+if [ "$#" -ge 3 ]
 then
-echo KC_HOSTNAME=$1 >> .env
-else
-echo KC_HOSTNAME=$(curl -s 2ip.ru).sslip.io >> .env
+    echo Error!!!
+    echo Too much parameters 
+    exit
+
+elif [ "$#" -eq 2 ]
+then
+    echo KC_HOSTNAME=$1 >> .env
+    sed -i '/EMAIL/d' .env
+    echo EMAIL=$2 >> .env
+elif [ "$#" -eq 1 ]
+then
+    email=$(echo "$1" | awk '/@/{print $0}')
+    if [ -n "$email" ]
+    then
+        echo KC_HOSTNAME=$(curl -s 2ip.ru).sslip.io >> .env
+        sed -i '/EMAIL/d' .env
+        echo EMAIL=$1 >> .env
+    else
+        source .env
+        if [ "$EMAIL" = "CHANGE_ME" ]
+        then
+            echo Error!!!
+            echo Need correct e-mail
+            exit
+        else
+            echo KC_HOSTNAME=$1 >> .env
+        fi
+    fi
+elif [ "$#" -eq 0 ]
+then
+    source .env
+    if [ "$EMAIL" = "CHANGE_ME" ]
+    then
+        echo Error!!!
+        echo Need correct e-mail
+        exit
+    else
+        echo KC_HOSTNAME=$(curl -s 2ip.ru).sslip.io >> .env
+    fi
 fi
 
-if [ -n "$2" ]
-then
-sed -i '/EMAIL/d' .env
-echo EMAIL=$2 >> .env
-else
-source .env
-if [ "$EMAIL" = "CHANGE_ME" ]
-then
-echo Need correct e-mail
-exit
-fi
+clear
+echo Check your config:
+echo
+cat .env
+echo
+echo -n "Is congfig correct? [y/N] "
+	read -e choice
+if [ "$choice" = "" ]
+    then restore_config
+elif [ "$choice" = "n" ]
+    then restore_config
+elif [ "$choice" = "N" ]
+    then restore_config
+elif [ "$choice" != "y" ]
+    then
+        if [ "$choice" != "Y" ]
+            then
+                echo Incorrect choice
+                sleep 5
+                restore_config
+        fi
 fi
  
 # Phase 1
@@ -36,6 +98,7 @@ openssl dhparam -out letsencrypt/ssl-dhparams.pem 2048
 cat <<EOF >./crontab
 # m h  dom mon dow   command
 0 0 1 * * $(pwd)/cron_job.sh
+
 EOF
 
 crontab ./crontab
